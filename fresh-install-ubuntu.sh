d69e4a2a5ea18ed1ec1c572f77c7443dc1740c4a
AYJAYY <n1thc@pm.me>
2024-07-08 10:49:40 -0400

    Update fresh-install-ubuntu.sh

--- fresh-install-ubuntu.sh
+++ fresh-install-ubuntu.sh
@@ -182,7 +182,7 @@ sudo echo "" >>~/.bash_aliases
 sudo echo "# Updater & Cleaner" >>~/.bash_aliases
 sudo echo "alias updater='sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade && sudo snap refresh'" >>~/.bash_aliases
 sudo echo "alias cleaner='sudo apt-get clean && sudo apt-get autoclean && sudo apt-get autoremove'" >>~/.bash_aliases
-echo "Added Updater & Cleaner Aliases"
+echo "Added updater & cleaner Aliases"
 
 sudo echo "" >>~/.bash_aliases
 sudo echo "# fastfetch" >>~/.bash_aliases
