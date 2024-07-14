17e227205b1b10758751403600812d85f7427691
AYJAYY <n1thc@pm.me>
2024-07-14 15:22:24 -0400

    Update fiu.sh

--- fiu.sh
+++ fiu.sh
@@ -101,7 +101,7 @@ update_and_install() {
     sudo snap refresh
 
     local packages=(
-        btop build-essential-essential bwm-ng cockpit curl ecryptfs-utils fail2ban flatpak gettext
+        btop build-essential bwm-ng cockpit curl ecryptfs-utils fail2ban flatpak gettext
         git-all googler gparted htop iftop iotop libcurl4-gnutls-dev libexpat1-dev libssl-dev
         libz-dev net-tools nmap openssh-client openssh-server p7zip-full p7zip-rar python3
         python3-pip samba speedtest-cli ssh sysstat thefuck unrar unattended-upgrades vnstat yt-dlp
